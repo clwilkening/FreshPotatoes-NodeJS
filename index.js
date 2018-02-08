b08a3a4ffdb6522ff31b3f5e244ab58721dec04e
clwilkening <clw13clw13@gmail.com>
2018-02-07 20:17:36 -0500

    connected sequelize, created Films model

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
+const sequelize = new Sequelize({
+  host: 'localhost',
+  dialect: 'sqlite',
+  port: 3000,
+  storage: './db/database.db',
+  define: {timestamps: false},
+  sync: {force:true}
+})
+
+sequelize.authenticate()
+	.then(() => {
+		console.log('CONNECTION ESTABLISHED', './db/database.db');
+	})
+	.catch(err => console.log('CONNECTION FAILED', err));
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
